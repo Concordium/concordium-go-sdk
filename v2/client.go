4f2d7cf0f020feff3d337cdeb557c728fc5ba9e1
Aleš Bizjak <ab@concordium.com>
2023-08-11 20:43:44 +0200

    Revise the client to accept the list of root certificates.

--- v2/client.go
+++ v2/client.go
@@ -3,26 +3,16 @@ package v2
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"errors"
-	"os"
-
+	"github.com/BoostyLabs/concordium-go-sdk/v2/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
-
-	"github.com/BoostyLabs/concordium-go-sdk/v2/pb"
 )
 
 // Config contains Concordium configurable values.
 type Config struct {
-	NodeAddress string `env:"NODE_ADDRESS"`
-	CA          struct {
-		Enabled               bool   `env:"CA_ENABLED" envDefault:"false"`
-		InsecureSkipVerify    bool   `env:"CA_INSECURE_SKIP_VERIFY" envDefault:"false"`
-		CACertificatePath     string `env:"CA_CA_CERTIFICATE_PATH" envDefault:""`
-		ClientCertificatePath string `env:"CA_CLIENT_CERTIFICATE_PATH" envDefault:""`
-		ClientCertificateKey  string `env:"CA_CLIENT_CERTIFICATE_KEY_PATH" envDefault:""`
-	}
+	NodeAddress    string `env:"NODE_ADDRESS"`
+	TlsCredentials credentials.TransportCredentials
 }
 
 // Client provides grpc connection with node.
@@ -34,54 +24,38 @@ type Client struct {
 
 // NewClient creates new concordium grpc client.
 func NewClient(config Config) (_ *Client, err error) {
-	creds, err := loadTLSCredentials(config)
-	if err != nil {
-		return nil, err
+	if config.TlsCredentials != nil {
+		conn, err := grpc.Dial(
+			config.NodeAddress,
+			grpc.WithTransportCredentials(config.TlsCredentials),
+		)
+		if err != nil {
+			return nil, err
+		}
+		client := pb.NewQueriesClient(conn)
+
+		return &Client{GrpcClient: client, ClientConn: conn, config: config}, nil
+	} else {
+		conn, err := grpc.Dial(
+			config.NodeAddress,
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		)
+		if err != nil {
+			return nil, err
+		}
+		client := pb.NewQueriesClient(conn)
+
+		return &Client{GrpcClient: client, ClientConn: conn, config: config}, nil
 	}
-
-	conn, err := grpc.Dial(
-		config.NodeAddress,
-		grpc.WithTransportCredentials(creds),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	client := pb.NewQueriesClient(conn)
-
-	return &Client{GrpcClient: client, ClientConn: conn, config: config}, nil
 }
 
-// loadTLSCredentials load the certificate of the CA who signed the server’s certificate.
-func loadTLSCredentials(config Config) (credentials.TransportCredentials, error) {
-	if !config.CA.Enabled {
-		return insecure.NewCredentials(), nil
-	}
-
-	if config.CA.InsecureSkipVerify {
-		return credentials.NewTLS(&tls.Config{
-			InsecureSkipVerify: config.CA.InsecureSkipVerify,
-		}), nil
-	}
-
-	pemServerCA, err := os.ReadFile(config.CA.CACertificatePath)
+func HostTLSRoots() (_ credentials.TransportCredentials, err error) {
+	RootCAs, err := x509.SystemCertPool()
 	if err != nil {
 		return nil, err
 	}
-
-	certPool := x509.NewCertPool()
-	if !certPool.AppendCertsFromPEM(pemServerCA) {
-		return nil, errors.New("failed to add server CA's certificate")
-	}
-
-	clientCert, err := tls.LoadX509KeyPair(config.CA.ClientCertificatePath, config.CA.ClientCertificateKey)
-	if err != nil {
-		return nil, err
-	}
-
 	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{clientCert},
-		RootCAs:      certPool,
+		RootCAs: RootCAs,
 	}
 
 	return credentials.NewTLS(tlsConfig), nil
--- v2/examples/getaccountlist/main.go
+++ v2/examples/getaccountlist/main.go
@@ -10,7 +10,14 @@ import (
 
 // in this example we receive and print all accounts in base58 format that we received from specific block but absolute height.
 func main() {
-	client, err := v2.NewClient(v2.Config{NodeAddress: "node.testnet.concordium.com:20000"})
+
+	creds, err := v2.HostTLSRoots()
+
+	if err != nil {
+		return
+	}
+
+	client, err := v2.NewClient(v2.Config{NodeAddress: "grpc.testnet.concordium.com:20000", TlsCredentials: creds})
 
 	// sending empty context, can also use any other context instead, best block can be changed with any other type that implement isBlockHashInput.
 	accounts, err := client.GetAccountList(context.TODO(), v2.BlockHashInputBest{})
